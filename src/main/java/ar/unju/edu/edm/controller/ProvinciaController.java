6977b7084fee0ffdc52ba179220ab90fea7c337c
Marcos <Marcos@DESKTOP-KHEO9DL>
2021-04-09 15:54:01 -0300

    TP1-Version 9/4/2021 (Sin mensaje de error en division entre 0)

---
+++ src/main/java/ar/unju/edu/edm/controller/ProvinciaController.java
@@ -0,0 +1,24 @@
+package ar.unju.edu.edm.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ar.unju.edu.edm.model.Provincia;
+@Controller
+public class ProvinciaController {
+	
+	@Autowired
+	Provincia unaProvincia;
+	
+	@GetMapping({"/provincia"})
+	public String cargarProvincia(Model model) {
+		unaProvincia.setNombreDeProvincia("Salta, Argentina");
+		model.addAttribute("marcos", unaProvincia);
+		return "provincia";
+	}
+	
+	
+	
+}
